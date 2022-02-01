061ecf87293ffc7f4033bd0221b9536d3cc1b07b
Edmund Wohlmuth <edmundwoh@gmail.com>
2022-02-01 10:16:50 -0400

    cleaned up math

--- Assets/scripts/AIScript.cs
+++ Assets/scripts/AIScript.cs
@@ -4,38 +4,52 @@ using UnityEngine;
 
 public class AIScript : MonoBehaviour
 {
-    float speed = -2f;
+    public float speed = -2f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         gameObject.transform.Translate(0, 0, speed * Time.deltaTime);
+
+        // jumping
+        Random.Range(1, 20);
+
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision) // collision detection
     {
-        if (collision.gameObject.tag == "floor")
+        if (collision.gameObject.tag == "floor") // ignore floor collision
         {
             return;
         }
 
         Debug.Log("I hit something"); 
 
-        if (speed < 0)
+        if (speed < 0) // on colliding anything that isn't the floor
         {
-            speed = speed + speed * speed;
+            speed = speed *-1;
         }
         else if (speed > 0)
         {
-            speed = speed + -speed + -speed;
+            speed = speed *-1;
+        }       
+    }
+
+    private void OnTriggerEnter(Collider other) // 'GoombaStomping' the NotAGoomba
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Get stomped, you rube");
+            speed = 0f;
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+            gameObject.GetComponent<BoxCollider>().enabled = false;
         }
-        
     }
 }
